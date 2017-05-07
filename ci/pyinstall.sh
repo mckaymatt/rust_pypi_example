7ce72ccc2a5e0eb3b262d4dc8446c05b0248f3d6
mckaymatt <mckaymatt@gmail.com>
2017-05-07 17:10:45 -0400

    fix caching

--- ci/pyinstall.sh
+++ ci/pyinstall.sh
@@ -1,17 +1,22 @@
 set -ex
+
+pyenv --version || true
+
+# get most up to date pyenv but keep cache of Python versions
 rm -rf ~/.pyenv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 mkdir -p ~/.cache/pyenv/versions
 ln -s ~/.cache/pyenv/versions ~/.pyenv/versions
 export PATH="$HOME/.pyenv/shims:$HOME/.pyenv/bin:$PATH"
+pyenv --version
 pyenv install --skip-existing $PYENV
 pyenv global $PYENV
-pyenv --version
 
 pyenv rehash
 
 python --version
 pip --version
+
 pip install -U pip
 pip install -U virtualenv
 python -m virtualenv ~/.venv
--- ci/pyscript.sh
+++ ci/pyscript.sh
@@ -2,10 +2,13 @@
 set -ex
 
 python --version
-
 source ~/.venv/bin/activate
 python --version
+
 pip install -U pip
 pip install -r requirements_dev.txt
 make test 
 make lint
+
+python setup.py bdist_wheel
+ls -la dist
