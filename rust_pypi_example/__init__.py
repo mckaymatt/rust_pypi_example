78c6a007efa729b1109abb9d86cea12ca072c957
mckaymatt <mckaymatt@gmail.com>
2017-06-17 18:12:22 -0400

    Bump version: 0.6.7 → 0.6.8

--- rust_pypi_example/__init__.py
+++ rust_pypi_example/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Matt McKay"""
 __email__ = 'mckaymatt@gmail.com'
-__version__ = '0.6.7'
+__version__ = '0.6.8'
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setup(
     name='rust_pypi_example',
-    version='0.6.7',
+    version='0.6.8',
     description="Python Boilerplate contains all the boilerplate you need to create a Python wheel with Rust.",
     long_description=readme + '\n\n' + history,
     author="Matt McKay",
