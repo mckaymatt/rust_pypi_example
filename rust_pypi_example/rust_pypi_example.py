88045ffa6088849ec872a68c584eca45a15cb3b6
mckaymatt <mckaymatt@gmail.com>
2017-06-20 23:00:37 -0400

    fix path to dylib

--- rust_pypi_example/rust_pypi_example.py
+++ rust_pypi_example/rust_pypi_example.py
@@ -42,3 +42,49 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# -*- coding: utf-8 -*-
+import os.path
+import os
+import sys
+from cffi import FFI
+
+# See https://github.com/SimonSapin/hello-pyrust
+
+
+if sys.platform == 'win32':
+    DYNAMIC_LIB_FORMAT = '%s.dll'
+elif sys.platform == 'darwin':
+    DYNAMIC_LIB_FORMAT = 'lib%s.dylib'
+elif "linux" in sys.platform:
+    DYNAMIC_LIB_FORMAT = 'lib%s.so'
+else:
+    raise NotImplementedError('No implementation for "{}".'
+                              ' Supported platforms are '
+                              '"win32", "darwin", and "linux"'
+                              ''.format(sys.platform))
+ffi = FFI()
+(file_path, _) = os.path.split(__file__)
+cwd = os.getcwd()
+
+h_path = os.path.join(file_path, 'rust', 'src', 'rust_pypi_example.h',)
+h_rel_path = os.path.join(os.curdir, os.path.relpath(h_path, cwd))
+
+dlib_path = os.path.join(file_path, 'rust', 'target', 'release', 
+                         DYNAMIC_LIB_FORMAT % 'rust_pypi_example')
+dlib_rel_path = os.path.join(os.curdir, os.path.relpath(dlib_path, cwd))
+
+with open(h_rel_path) as h:
+    ffi.cdef(h.read())
+
+rust_lib = ffi.dlopen(dlib_rel_path)
+
+
+def main():
+    assert rust_lib.is_prime(13) == 1
+    assert rust_lib.is_prime(12) == 0
+
+
+if __name__ == '__main__':
+    main()
